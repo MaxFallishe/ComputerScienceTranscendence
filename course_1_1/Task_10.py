72cce6eec5ec2849ea6dfaaeea7ed6d19662c894
Nikita B <n.borsov@sailet.pro>
2023-04-24 23:44:01 +0300

    Completely rewrote task number 10 to increase the speed of the code (unittests too)

--- course_1_1/Task_10.py
+++ course_1_1/Task_10.py
@@ -1,91 +1,65 @@
-import random
-from typing import *
+class PowerSet:
+    def __init__(self):
+        self.bucket_count = 50_000
+        self.buckets = [[] for _ in range(self.bucket_count)]
+        self.item_count = 0
 
+    def __iter__(self):
+        for bucket in self.buckets:
+            for item in bucket:
+                yield item
 
-class HashTable:
-    def __init__(self, sz, stp):
-        self.hash_size = sz
-        self.hash_step = stp
-        self.slots = [None] * self.hash_size
+    def __contains__(self, item):
+        bucket = self.hash_fun(item)
+        return item in bucket
 
     def hash_fun(self, value):
-        return ''.join(f'{ord(i):b}' for i in value).count('1') % self.hash_size
+        return self.buckets[hash(value) % self.bucket_count]
 
-
-    def seek_slot(self, value):
-        init_hash_ind = self.hash_fun(value)
-        while None in self.slots:
-            if self.slots[init_hash_ind] is None or self.slots[init_hash_ind] == value:
-                return init_hash_ind
-            init_hash_ind = (init_hash_ind + self.hash_step) % self.hash_size
-        return None
+    def size(self):
+        return self.item_count
 
     def put(self, value):
-        insrt_ind = self.seek_slot(value)
-        if insrt_ind is not None:
-            self.slots[insrt_ind] = value
-        return insrt_ind
+        bucket = self.hash_fun(value)
+        if value not in bucket:
+            bucket.append(value)
+            self.item_count += 1
 
+    def get(self, value):
+        bucket = self.hash_fun(value)
+        return value in bucket
 
-    def find(self, value):
-        for slot_ind in range(len(self.slots)):
-            if self.slots[slot_ind] == value:
-                return slot_ind
-        return None
+    def remove(self, value):
+        bucket = self.hash_fun(value)
+        if value in bucket:
+            bucket.remove(value)
+            return True
+        return False
 
+    def intersection(self, set2):
+        result = PowerSet()
+        for item in self:
+            if item in set2:
+                result.put(item)
+        return result
 
-class PowerSet(HashTable):
-    def __init__(self, hash_size=100_000, hash_step=3):
-        super().__init__(hash_size, hash_step)
+    def union(self, set2):
+        result = PowerSet()
+        for item in self:
+            result.put(item)
+        for item in set2:
+            result.put(item)
+        return result
 
-    def size(self) -> int:
-        return len([slot for slot in self.slots if slot is not None])
+    def difference(self, set2):
+        result = PowerSet()
+        for item in self:
+            if item not in set2:
+                result.put(item)
+        return result
 
-    def seek_slot(self, value: str) -> Optional[int]:
-        init_hash_ind = self.hash_fun(value)
-        while None in self.slots and self.slots[init_hash_ind] != value:
-            if self.slots[init_hash_ind] is None or self.slots[init_hash_ind] == value:
-                return init_hash_ind
-            init_hash_ind = (init_hash_ind + self.hash_step) % self.hash_size
-        return None
-
-    def put(self, value: str) -> None:
-        insrt_ind = self.seek_slot(value)
-        if insrt_ind is not None:
-            self.slots[insrt_ind] = value
-
-    def get(self, value: str) -> bool:
-        return value in self.slots
-
-    def remove(self, value: str) -> bool:
-        init_hash_ind = self.hash_fun(value)
-        while self.slots[init_hash_ind] != value:
-            if self.slots[init_hash_ind] is None:
-                return False
-            init_hash_ind = (init_hash_ind + self.hash_step) % self.hash_size
-        self.slots[init_hash_ind] = None
-        return True
-
-    def intersection(self, set2: 'PowerSet') -> 'PowerSet':
-        intersec_pset = PowerSet()
-        [intersec_pset.put(slot) for slot in self.slots if slot in set2.slots and slot is not None]
-        return intersec_pset
-
-    def union(self, set2: 'PowerSet') -> 'PowerSet':
-        union_pset = PowerSet()
-        union_pset.slots = self.slots
-        [union_pset.put(slot) for slot in set2.slots if slot is not None]
-        return union_pset
-
-    def difference(self, set2: 'PowerSet') -> 'PowerSet':
-        diff_pset = PowerSet()
-        [diff_pset.put(slot) for slot in self.slots if slot not in set2.slots and slot is not None]
-        return diff_pset
-
-    def issubset(self, set2: 'PowerSet') -> bool:
-        if self.size() == 0:
-            return False
-        for slot in [i for i in set2.slots if i is not None]:
-            if slot not in self.slots:
+    def issubset(self, set2):
+        for item in set2:
+            if item not in self:
                 return False
         return True
--- course_1_1/tests/test_Task_10.py
+++ course_1_1/tests/test_Task_10.py
@@ -1,233 +1,239 @@
 from ..Task_10 import *
 import unittest
+import time
+import random
 
 
 class TestTask10(unittest.TestCase):
     # TEST SIZE() METHOD
     def test_size__1(self):
-        pset = PowerSet(19, 3)
+        pset = PowerSet()
         self.assertEqual(pset.size(), 0)
 
     def test_size__2(self):
-        pset = PowerSet(19, 3)
+        pset = PowerSet()
         pset.put('Goal')
         self.assertEqual(pset.size(), 1)
 
     def test_size__3(self):
-        pset = PowerSet(19, 3)
+        pset = PowerSet()
         pset.put('Goal')
         pset.put('Go')
         pset.put('Gong')
         pset.put('Goal')
         self.assertEqual(pset.size(), 3)
 
-    # TEST PUT() METHOD
-    def test_put__1(self):
-        pset = PowerSet(19, 3)
-        pset.put('Goal')  # hash = 17
-        self.assertEqual(pset.slots[17], 'Goal')
-
-    def test_put__2(self):
-        pset = PowerSet(19, 3)
-        pset.put('Goal')  # hash = 17
-        pset.put('Ball')  # hash = 13
-        res = (pset.slots[17], pset.slots[13])
-        ref = ('Goal', 'Ball')
-        self.assertEqual(res, ref)
-
-    def test_put__3(self):
-        pset = PowerSet(19, 3)
-        pset.put('Goal')  # hash = 17
-        pset.put('Ball')  # hash = 13
-        pset.put('Gtyf')  # hash = 17
-        res = (pset.slots[17], pset.slots[13], pset.slots[1])
-        ref = ('Goal', 'Ball', 'Gtyf')
-        self.assertEqual(res, ref)
-
-    # TEST GET() METHOD
+    # # TEST GET() METHOD
     def test_get__1(self):
-        pset = PowerSet(19, 3)
-        pset.put('Goal')  # hash = 17
-        pset.put('Ball')  # hash = 13
-        pset.put('Gtyf')  # hash = 17
+        pset = PowerSet()
+        pset.put('Goal')
+        pset.put('Ball')
+        pset.put('Gtyf')
         self.assertTrue(pset.get('Goal'))
 
     def test_get__2(self):
-        pset = PowerSet(19, 3)
-        pset.put('Goal')  # hash = 17
-        pset.put('Ball')  # hash = 13
-        pset.put('Gtyf')  # hash = 17
+        pset = PowerSet()
+        pset.put('Goal')
+        pset.put('Ball')
+        pset.put('Gtyf')
         self.assertFalse(pset.get('Ogo'))
 
     def test_get__3(self):
-        pset = PowerSet(19, 3)
+        pset = PowerSet()
         self.assertFalse(pset.get('Ogo'))
 
-    # TEST REMOVE() METHOD
+    # TEST PUT() METHOD
+    def test_put__1(self):
+        pset = PowerSet()
+        pset.put('Goal')
+        self.assertTrue(pset.get('Goal'))
+
+    def test_put__2(self):
+        pset = PowerSet()
+        pset.put('Goal')  # hash = 17
+        pset.put('Ball')  # hash = 13
+        self.assertTrue(pset.get('Ball'))
+
+    def test_put__3(self):
+        pset = PowerSet()
+        pset.put('Goal')
+        pset.put('Ball')
+        pset.put('Gtyf')
+        self.assertTrue(pset.get('Goal'))
+
+    def test_put__4(self):
+        pset = PowerSet()
+        pset.put('Goal')
+        pset.put('Goal')
+        pset.put('Ball')
+        pset.put('Ball')
+        self.assertEqual(pset.size(), 2)
+
+    # # TEST REMOVE() METHOD
     def test_remove__1(self):
-        pset = PowerSet(19, 3)
+        pset = PowerSet()
         self.assertFalse(pset.remove('Ogo'))
 
     def test_remove__2(self):
-        pset = PowerSet(19, 3)
-        pset.put('Goal')  # hash = 17
-        pset.put('Ball')  # hash = 13
-        pset.put('Gtyf')  # hash = 17
+        pset = PowerSet()
+        pset.put('Goal')
+        pset.put('Ball')
+        pset.put('Gtyf')
         res = (pset.remove('Goal'), pset.get('Goal'))
         ref = (True, False)
         self.assertEqual(res, ref)
 
     def test_remove__3(self):
-        pset = PowerSet(19, 3)
-        pset.put('Goal')  # hash = 17
-        pset.put('Ball')  # hash = 13
-        pset.put('Gtyf')  # hash = 17
+        pset = PowerSet()
+        pset.put('Goal')
+        pset.put('Ball')
+        pset.put('Gtyf')
         res = (pset.remove('Goal'), pset.remove('Goal'))
         ref = (True, False)
         self.assertEqual(res, ref)
 
     def test_remove__4(self):
-        pset = PowerSet(19, 3)
-        pset.put('Goal')  # hash = 17
-        pset.put('Ball')  # hash = 13
-        pset.put('Gtyf')  # hash = 17
+        pset = PowerSet()
+        pset.put('Goal')
+        pset.put('Ball')
+        pset.put('Gtyf')
         res = (pset.remove('Gtyf'), pset.remove('Gtyf'))
         ref = (True, False)
         self.assertEqual(res, ref)
 
     # TEST INTERSECTION() METHOD
     def test_intersection__1(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
         pset1.put('k11')
         pset1.put('k8')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k2')
         pset2.put('k1')
         pset2.put('k8')
         pset2.put('k6')
 
         powset3 = pset1.difference(pset2)
-        result = set(i for i in powset3.slots if i is not None)
+        result = set(item for i in powset3.buckets for item in i if item is not None)
         self.assertEqual(result, {'k11'})
 
     def test_intersection__2(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
         pset1.put('k6')
         pset1.put('k8')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k2')
         pset2.put('k1')
         pset2.put('k8')
         pset2.put('k6')
 
         powset3 = pset1.difference(pset2)
-        result = set([i for i in powset3.slots if i is not None])
+        result = set(item for i in powset3.buckets for item in i if item is not None)
         self.assertEqual(result, set())
 
     def test_intersection__3(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
         pset1.put('k99')
         pset1.put('k88')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k2')
         pset2.put('k1')
         pset2.put('k8')
         pset2.put('k6')
 
         powset3 = pset1.difference(pset2)
-        result = set(i for i in powset3.slots if i is not None)
+        result = set(item for i in powset3.buckets for item in i if item is not None)
         self.assertEqual(result, {'k88', 'k99'})
 
     # TEST UNION() METHOD
     def test_union__1(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
         pset1.put('k99')
         pset1.put('k88')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k2')
         pset2.put('k1')
         pset2.put('k8')
         pset2.put('k6')
 
         powset3 = pset1.union(pset2)
-        result = set(i for i in powset3.slots if i is not None)
+        result = set(item for i in powset3.buckets for item in i if item is not None)
         self.assertEqual(result, {'k1', 'k2', 'k99', 'k88', 'k8', 'k6'})
 
     def test_union__2(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k2')
         pset2.put('k1')
 
         powset3 = pset1.union(pset2)
-        result = set(i for i in powset3.slots if i is not None)
+        result = set(item for i in powset3.buckets for item in i if item is not None)
         self.assertEqual(result, {'k1', 'k2'})
 
     def test_union__3(self):
-        pset1 = PowerSet(19, 3)
-        pset2 = PowerSet(19, 3)
+        pset1 = PowerSet()
+        pset2 = PowerSet()
 
         powset3 = pset1.union(pset2)
-        result = set(i for i in powset3.slots if i is not None)
+        result = set(item for i in powset3.buckets for item in i if item is not None)
         self.assertEqual(result, set())
 
     # TEST DIFFERENCE() METHOD
     def test_difference__1(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
         pset1.put('k3')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k2')
         pset2.put('k1')
 
         powset3 = pset1.difference(pset2)
-        result = set(i for i in powset3.slots if i is not None)
+        result = set(item for i in powset3.buckets for item in i if item is not None)
         self.assertEqual(result, {'k3'})
 
     def test_difference__2(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
         pset1.put('k3')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k5')
         pset2.put('k6')
 
         powset3 = pset1.difference(pset2)
-        result = set(i for i in powset3.slots if i is not None)
+        result = set(item for i in powset3.buckets for item in i if item is not None)
         self.assertEqual(result, {'k1', 'k2', 'k3'})
 
     def test_difference__3(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k6')
         pset1.put('k5')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k5')
         pset2.put('k6')
 
         powset3 = pset1.difference(pset2)
-        result = set(i for i in powset3.slots if i is not None)
+        result = set(item for i in powset3.buckets for item in i if item is not None)
         self.assertEqual(result, set())
 
     # TEST ISSUBSET() METHOD
     def test_issubset__1(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
         pset1.put('k3')
         pset1.put('k4')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k1')
         pset2.put('k2')
 
@@ -235,8 +241,8 @@ class TestTask10(unittest.TestCase):
         self.assertTrue(result)
 
     def test_issubset__2(self):
-        pset1 = PowerSet(19, 3)
-        pset2 = PowerSet(19, 3)
+        pset1 = PowerSet()
+        pset2 = PowerSet()
         pset2.put('k1')
         pset2.put('k2')
 
@@ -244,12 +250,12 @@ class TestTask10(unittest.TestCase):
         self.assertFalse(result)
 
     def test_issubset__3(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
         pset1.put('k3')
         pset1.put('k4')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k1')
 
         result = pset1.difference(pset2)
@@ -257,12 +263,12 @@ class TestTask10(unittest.TestCase):
 
 
     def test_issubset__4(self):
-        pset1 = PowerSet(100, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
         pset1.put('k3')
         pset1.put('k4')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k1')
 
         result = pset1.difference(pset2)
@@ -270,14 +276,115 @@ class TestTask10(unittest.TestCase):
 
 
     def test_issubset__5(self):
-        pset1 = PowerSet(19, 3)
+        pset1 = PowerSet()
         pset1.put('k1')
         pset1.put('k2')
         pset1.put('k3')
         pset1.put('k4')
-        pset2 = PowerSet(19, 3)
+        pset2 = PowerSet()
         pset2.put('k5')
         pset2.put('k1')
 
         result = pset1.issubset(pset2)
         self.assertFalse(result)
+
+    # Checking for the performance of functions
+    # (operations on sets of tens of thousands of elements fit in a couple of seconds)
+    def test_performance__init(self):
+        t1 = time.time()
+        pset1 = PowerSet()
+        dur = time.time() - t1
+        self.assertLess(dur, 2)
+
+    def test_performance__hashfun(self):
+        pset1 = PowerSet()
+        t1 = time.time()
+        pset1.hash_fun('test')
+        dur = time.time() - t1
+        self.assertLess(dur, 2)
+
+    def test_performance__size(self):
+        pset1 = PowerSet()
+        for i in range(30_000):
+            pset1.put(str(random.randint(0, 10_000_000)))
+        t1 = time.time()
+        sz = pset1.size()
+        dur = time.time() - t1
+        self.assertLess(dur, 2)
+
+    def test_performance__put(self):
+        pset1 = PowerSet()
+        for i in range(30_000):
+            pset1.put(str(random.randint(0, 10_000_000)))
+        t1 = time.time()
+        pset1.put('Goal')
+        dur = time.time() - t1
+        self.assertLess(dur, 2)
+
+    def test_performance__get(self):
+        pset1 = PowerSet()
+        for i in range(30_000):
+            pset1.put(str(random.randint(0, 10_000_000)))
+        pset1.put('Goal')
+        t1 = time.time()
+        pset1.get('Goal')
+        dur = time.time() - t1
+        self.assertLess(dur, 2)
+
+    def test_performance__remove(self):
+        pset1 = PowerSet()
+        for i in range(30_000):
+            pset1.put(str(random.randint(0, 10_000_000)))
+        pset1.put('Goal')
+        t1 = time.time()
+        pset1.remove('Goal')
+        dur = time.time() - t1
+        self.assertLess(dur, 2)
+
+    def test_performance__intersection(self):
+        pset1 = PowerSet()
+        pset2 = PowerSet()
+        for i in range(30_000):
+            pset1.put(str(random.randint(0, 10_000_000)))
+        for i in range(30_000):
+            pset2.put(str(random.randint(0, 10_000_000)))
+        t1 = time.time()
+        pset1.intersection(pset2)
+        dur = time.time() - t1
+        self.assertLess(dur, 2)
+
+    def test_performance__union(self):
+        pset1 = PowerSet()
+        pset2 = PowerSet()
+        for i in range(30_000):
+            pset1.put(str(random.randint(0, 10_000_000)))
+        for i in range(30_000):
+            pset2.put(str(random.randint(0, 10_000_000)))
+        t1 = time.time()
+        pset1.union(pset2)
+        dur = time.time() - t1
+        self.assertLess(dur, 2)
+
+    def test_performance__difference(self):
+        pset1 = PowerSet()
+        pset2 = PowerSet()
+        for i in range(30_000):
+            pset1.put(str(random.randint(0, 10_000_000)))
+        for i in range(30_000):
+            pset2.put(str(random.randint(0, 10_000_000)))
+        t1 = time.time()
+        pset1.difference(pset2)
+        dur = time.time() - t1
+        self.assertLess(dur, 2)
+
+    def test_performance__issubset(self):
+        pset1 = PowerSet()
+        pset2 = PowerSet()
+        for i in range(30_000):
+            pset1.put(str(random.randint(0, 10_000_000)))
+        for i in range(30_000):
+            pset2.put(str(random.randint(0, 10_000_000)))
+        t1 = time.time()
+        pset1.issubset(pset2)
+        dur = time.time() - t1
+        self.assertLess(dur, 2)
