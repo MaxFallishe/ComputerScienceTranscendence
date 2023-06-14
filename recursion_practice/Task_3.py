a35cbc2ee7d82ca71d19fb5e46eb5a35bc3bdbed
Nikita B <n13b@inbox.ru>
2023-06-14 15:34:00 +0300

    Added third task (recursive list len calculation) with unittests (5+)

---
+++ recursion_practice/Task_3.py
@@ -0,0 +1,6 @@
+def get_list_len(lst: list) -> int:
+    try:
+        lst.pop(0)
+        return 1 + get_list_len(lst)
+    except IndexError as e:
+        return 0
---
+++ recursion_practice/tests/test_Task_3.py
@@ -0,0 +1,30 @@
+import unittest
+from ..Task_3 import *
+
+
+class TestTask3(unittest.TestCase):
+    # ### TEST GET_NUMBER_DIGITS_SUM() FUNCTION
+    def test_get_list_len__1(self):
+        result = get_list_len([1, 2, 3])
+        self.assertEqual(result, 3)
+
+    def test_get_list_len__2(self):
+        result = get_list_len([1])
+        self.assertEqual(result, 1)
+
+    def test_get_list_len__3(self):
+        result = get_list_len([])
+        self.assertEqual(result, 0)
+
+    def test_get_list_len__4(self):
+        result = get_list_len([1, "2", None, {}])
+        self.assertEqual(result, 4)
+
+    def test_get_list_len__5(self):
+        result = get_list_len([None, None])
+        self.assertEqual(result, 3)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
