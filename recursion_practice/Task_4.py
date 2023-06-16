ce45d78ae531219b23b12cc3c5f2953fffc34585
Nikita B <n13b@inbox.ru>
2023-06-16 18:01:30 +0300

    Refactor fourth task (remove slices in solution)

--- recursion_practice/Task_4.py
+++ recursion_practice/Task_4.py
@@ -1,6 +1,7 @@
-def is_string_palindrome(text: str) -> bool:
-    if len(text) < 1:
+def is_string_palindrome(text: str, start_indx=0) -> bool:
+    if start_indx >= len(text) / 2:
         return True
-    if text[0] != text[-1]:
-        return False
-    return is_string_palindrome(text[1:-1])
+    if text[start_indx] == text[(start_indx*-1)-1]:
+        return is_string_palindrome(text, start_indx+1)
+    return False
+
