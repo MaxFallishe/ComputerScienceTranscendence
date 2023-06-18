fd3326829ea2212ea7122ca4558b2b07748a7dac
Nikita B <n13b@inbox.ru>
2023-06-18 16:58:20 +0300

    Refactor sixth task (decomposition of a recursive function into two)

--- recursion_practice/Task_6.py
+++ recursion_practice/Task_6.py
@@ -1,9 +1,11 @@
-def print_even_indexed_items(lst: list, even_index_flag=None) -> None:
-    if even_index_flag is None:
-        even_index_flag = 1
+def print_even_indexed_items(lst: list) -> None:
+    print_recursively_even_indexed_items(lst, 1)
+
+
+def print_recursively_even_indexed_items(lst: list, even_index_flag: int) -> None:
     if not len(lst):
         return
     popped_item = lst.pop(0)
     if even_index_flag + 1:
         print(popped_item, end=' ')
-    print_even_indexed_items(lst, even_index_flag=even_index_flag*-1)
+    print_recursively_even_indexed_items(lst, even_index_flag * -1)
