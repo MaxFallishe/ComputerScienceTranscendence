dd466765cfd77102d8382c5094ddfe4cc0ba765e
Nikita B <n13b@inbox.ru>
2023-06-18 17:07:03 +0300

    Refactor fourth task (decomposition of a recursive function into two)

--- recursion_practice/Task_4.py
+++ recursion_practice/Task_4.py
@@ -1,9 +1,11 @@
-def is_string_palindrome(text: str, start_indx=None) -> bool:
-    if start_indx is None:
-        start_indx = 0
+def is_string_palindrome(text: str) -> bool:
+    return is_string_palindrome_recurcively(text, 0)
+
+
+def is_string_palindrome_recurcively(text: str, start_indx: int) -> bool:
     if start_indx >= len(text) / 2:
         return True
     if text[start_indx] == text[(start_indx*-1)-1]:
-        return is_string_palindrome(text, start_indx+1)
+        return is_string_palindrome_recurcively(text, start_indx+1)
     return False
 
