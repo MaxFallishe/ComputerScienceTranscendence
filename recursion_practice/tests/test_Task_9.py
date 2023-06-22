a9301c431acd9f38a910e85c9cdc1d2cd084648b
Nikita B <n13b@inbox.ru>
2023-06-22 03:20:10 +0300

    Update testing function name in unittests

--- recursion_practice/tests/test_Task_9.py
+++ recursion_practice/tests/test_Task_9.py
@@ -4,57 +4,57 @@ from ..Task_9 import *
 
 
 class TestTask9(unittest.TestCase):
-    # ### TEST GENERATE_PARENTHESIS() FUNCTION
-    def test_generate_parenthesis__1(self):
-        result = generate_parenthesis(0)
+    # ### TEST generate_parenthesis_front() FUNCTION
+    def test_generate_parenthesis_front__1(self):
+        result = generate_parenthesis_front(0)
         self.assertEqual(result, None)
 
-    def test_generate_parenthesis__2(self):
-        self.assertRaises(TypeError, len, generate_parenthesis(0))
+    def test_generate_parenthesis_front__2(self):
+        self.assertRaises(TypeError, len, generate_parenthesis_front(0))
 
-    def test_generate_parenthesis__3(self):
-        result = generate_parenthesis(1)
+    def test_generate_parenthesis_front__3(self):
+        result = generate_parenthesis_front(1)
         reference = ["()"]
         self.assertEqual(result, reference)
 
-    def test_generate_parenthesis__4(self):
-        result = len(generate_parenthesis(1))
+    def test_generate_parenthesis_front__4(self):
+        result = len(generate_parenthesis_front(1))
         reference = 1
         self.assertEqual(result, reference)
 
-    def test_generate_parenthesis__5(self):
-        result = generate_parenthesis(2)
+    def test_generate_parenthesis_front__5(self):
+        result = generate_parenthesis_front(2)
         reference = ["(())", "()()"]
         result = set(result)
         reference = set(reference)
         self.assertEqual(result, reference)
 
-    def test_generate_parenthesis__6(self):
-        result = len(generate_parenthesis(2))
+    def test_generate_parenthesis_front__6(self):
+        result = len(generate_parenthesis_front(2))
         reference = 2
         self.assertEqual(result, reference)
 
-    def test_generate_parenthesis__7(self):
-        result = generate_parenthesis(3)
+    def test_generate_parenthesis_front__7(self):
+        result = generate_parenthesis_front(3)
         reference = ["()()()", "()(())", "(())()", "(()())", "((()))"]
         result = set(result)
         reference = set(reference)
         self.assertEqual(result, reference)
 
-    def test_generate_parenthesis__8(self):
-        result = len(generate_parenthesis(3))
+    def test_generate_parenthesis_front__8(self):
+        result = len(generate_parenthesis_front(3))
         reference = 5
         self.assertEqual(result, reference)
 
-    def test_generate_parenthesis__9(self):
-        result = generate_parenthesis(4)
+    def test_generate_parenthesis_front__9(self):
+        result = generate_parenthesis_front(4)
         reference = ["()()()()", "()()(())", "()(())()", "()(()())", "()((()))", "(())()()", "(())(())", "(()())()", "((()))()", "(()()())", "(()(()))", "((())())", "((()()))", "(((())))"]
         result = set(result)
         reference = set(reference)
         self.assertEqual(result, reference)
 
-    def test_generate_parenthesis__10(self):
-        result = len(generate_parenthesis(4))
+    def test_generate_parenthesis_front__10(self):
+        result = len(generate_parenthesis_front(4))
         reference = 14
         self.assertEqual(result, reference)
 
