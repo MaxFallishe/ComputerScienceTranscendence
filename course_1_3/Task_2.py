679b084c723b80c9b9f7c32894b52591f8b7c0e2
Nikita B <n13b@inbox.ru>
2023-06-28 00:36:59 +0300

    Added second task (implementation of the binary tree class) with unittests (50+)

---
+++ course_1_3/Task_2.py
@@ -0,0 +1,188 @@
+class BSTNode:
+    def __init__(self, key, val, parent):
+        self.NodeKey = key
+        self.NodeValue = val
+        self.Parent = parent
+        self.LeftChild = None
+        self.RightChild = None
+
+
+class BSTFind:
+    def __init__(self):
+        self.Node = None
+
+        self.NodeHasKey = False
+        self.ToLeft = False
+
+    def __str__(self):
+        node_representation = self.Node
+        if node_representation is not None:
+            node_representation = self.Node.NodeKey
+        return f"BSTFind obj\n'''\n{node_representation}\n{self.NodeHasKey}\n{self.ToLeft}\n'''"
+
+
+class BST:
+    def __init__(self, node):
+        self.Root = node
+
+    def FindNodeByKey(self, key):
+        if not self.Root:
+            bst_find = BSTFind()
+            return bst_find
+        return self.__find_node_by_key(key, self.Root)
+
+    def __find_node_by_key(self, key, node: BSTNode) -> BSTFind:
+        if node.NodeKey == key:
+            bst_find = BSTFind()
+            bst_find.Node = node
+            bst_find.NodeHasKey = True
+            return bst_find
+        elif node.NodeKey < key and node.RightChild is not None:
+            return self.__find_node_by_key(key, node.RightChild)
+        elif node.NodeKey < key and node.RightChild is None:
+            bst_find = BSTFind()
+            bst_find.Node = node
+            return bst_find
+        elif node.NodeKey > key and node.LeftChild is not None:
+            return self.__find_node_by_key(key, node.LeftChild)
+        elif node.NodeKey > key and node.LeftChild is None:
+            bst_find = BSTFind()
+            bst_find.Node = node
+            bst_find.ToLeft = True
+            return bst_find
+
+
+    def AddKeyValue(self, key, val):
+        bst_find = self.FindNodeByKey(key)
+        if bst_find.NodeHasKey:
+            return False
+        bst_node = BSTNode(key, val, bst_find.Node)
+        if bst_find.ToLeft:
+            bst_find.Node.LeftChild = bst_node
+        if not bst_find.ToLeft:
+            bst_find.Node.RightChild = bst_node
+
+    def FinMinMax(self, FromNode, FindMax):
+        if FromNode.LeftChild is None and FindMax is not True:
+            return FromNode
+        if FromNode.RightChild is None and FindMax is True:
+            return FromNode
+        if FromNode.LeftChild is not None and FindMax is not True:
+            return self.FinMinMax(FromNode.LeftChild, FindMax)
+        if FromNode.RightChild is not None and FindMax is True:
+            return self.FinMinMax(FromNode.RightChild, FindMax)
+
+
+    def DeleteNodeByKey(self, key):
+        bst_find = self.FindNodeByKey(key)
+        if not bst_find.NodeHasKey:
+            return False
+        if bst_find.Node is self.Root:
+            self.__delete_root_node()
+            return
+
+        if not bst_find.Node.LeftChild and not bst_find.Node.RightChild:
+            if bst_find.Node.Parent.LeftChild is bst_find.Node:
+                bst_find.Node.Parent.LeftChild = None
+            elif bst_find.Node.Parent.RightChild is bst_find.Node:
+                bst_find.Node.Parent.RightChild = None
+            bst_find.Node.Parent = None
+        elif bst_find.Node.LeftChild and not bst_find.Node.RightChild:
+            if bst_find.Node.Parent.LeftChild is bst_find.Node:
+                bst_find.Node.Parent.LeftChild = bst_find.Node.LeftChild
+            elif bst_find.Node.Parent.RightChild is bst_find.Node:
+                bst_find.Node.Parent.RightChild = bst_find.Node.LeftChild
+            bst_find.Node.Parent = None
+        elif not bst_find.Node.LeftChild and bst_find.Node.RightChild:
+            if bst_find.Node.Parent.LeftChild is bst_find.Node:
+                bst_find.Node.Parent.LeftChild = bst_find.Node.RightChild
+            elif bst_find.Node.Parent.RightChild is bst_find.Node:
+                bst_find.Node.Parent.RightChild = bst_find.Node.RightChild
+            bst_find.Node.Parent = None
+        elif bst_find.Node.LeftChild and bst_find.Node.RightChild:
+            new_root = self.FinMinMax(bst_find.Node.RightChild, False)
+            if new_root.RightChild:
+                if new_root.Parent.LeftChild is new_root:
+                    new_root.Parent.LeftChild = new_root.RightChild
+                    new_root.RightChild.Parent = new_root.Parent
+                elif new_root.Parent.RightChild is new_root:
+                    new_root.Parent.RightChild = new_root.RightChild
+                    new_root.RightChild.Parent = new_root.Parent
+
+            if new_root.Parent.LeftChild is new_root:
+                new_root.Parent.LeftChild = None
+            elif new_root.Parent.RightChild is new_root:
+                new_root.Parent.RightChild = None
+
+            if bst_find.Node.Parent.LeftChild is bst_find.Node:
+                bst_find.Node.Parent.LeftChild = new_root
+                new_root.Parent = bst_find.Node.Parent
+            elif bst_find.Node.Parent.RightChild is bst_find.Node:
+                bst_find.Node.Parent.RightChild = new_root
+                new_root.Parent = bst_find.Node.Parent
+
+            if new_root is not bst_find.Node.RightChild:
+                new_root.RightChild = bst_find.Node.RightChild
+            new_root.LeftChild = bst_find.Node.LeftChild
+            bst_find.Node.RightChild = None
+            bst_find.Node.LeftChild = None
+            bst_find.Node.Parent = None
+
+
+
+    def __delete_root_node(self) -> None:
+        if not self.Root.LeftChild and not self.Root.RightChild:
+            self.Root = None
+        elif self.Root.LeftChild and not self.Root.RightChild:
+            self.Root = self.Root.LeftChild
+            self.Root.Parent = None
+        elif not self.Root.LeftChild and self.Root.RightChild:
+            self.Root = self.Root.RightChild
+            self.Root.Parent = None
+        elif self.Root.LeftChild and self.Root.RightChild:
+            new_root = self.FinMinMax(self.Root.RightChild, False)
+            if new_root.RightChild:
+                if new_root.Parent.LeftChild is new_root:
+                    new_root.Parent.LeftChild = new_root.RightChild
+                    new_root.RightChild.Parent = new_root.Parent
+                elif new_root.Parent.RightChild is new_root:
+                    new_root.Parent.RightChild = new_root.RightChild
+                    new_root.RightChild.Parent = new_root.Parent
+
+            if new_root.Parent.LeftChild is new_root:
+                new_root.Parent.LeftChild = None
+            elif new_root.Parent.RightChild is new_root:
+                new_root.Parent.RightChild = None
+
+            if new_root is not self.Root.RightChild:
+                new_root.RightChild = self.Root.RightChild
+
+            new_root.LeftChild = self.Root.LeftChild
+
+            if new_root.LeftChild:
+                new_root.LeftChild.Parent = new_root
+
+            self.Root.RightChild = None
+            self.Root.LeftChild = None
+
+            self.Root = new_root
+            new_root.Parent = None
+
+
+
+    def Count(self):
+        return self.__count(self.Root)
+
+    def __count(self, node) -> int:
+        if node is None:
+            return 0
+        return 1 + self.__count(node.LeftChild) + self.__count(node.RightChild)
+
+
+def print_BST(node: BSTNode, count: int, node_type: str) -> None:
+    if node is None:
+        print(count * '     ' + f'|{node_type} ' + "-", None)
+        return
+    print(count * '     ' + f'|{node_type} ' + "-", node.NodeKey)
+    print_BST(node.LeftChild, count+1, 'l')
+    print_BST(node.RightChild, count+1, 'r')
---
+++ course_1_3/tests/test_Task_2.py
@@ -0,0 +1,517 @@
+from ..Task_2 import *
+import unittest
+
+
+class BSTMocksGenerator:
+    def __init__(self):
+        pass
+
+    def generate_bst(self) -> BST:
+        pass
+
+
+class BSTMocksGeneratorType1(BSTMocksGenerator):
+    """
+    Binary Strictly Tree object with following structure:
+                2
+               / \
+              1   3
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.node_2 = BSTNode(2, 'node2', None)
+        self.node_1 = BSTNode(1, 'node1', self.node_2)
+        self.node_3 = BSTNode(3, 'node3', self.node_2)
+
+    def generate_bst(self):
+        self.node_2.LeftChild = self.node_1
+        self.node_2.RightChild = self.node_3
+
+        bst = BST(self.node_2)
+        return bst
+
+
+class BSTMocksGeneratorType2(BSTMocksGenerator):
+    """
+    Binary Strictly Tree object with following structure:
+                 4
+               /  \
+              2    6
+             / \  / \
+            1  3 5  7
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.node_4 = BSTNode(4, 'node4', None)
+        self.node_2 = BSTNode(2, 'node2', self.node_4)
+        self.node_6 = BSTNode(6, 'node6', self.node_4)
+        self.node_1 = BSTNode(1, 'node1', self.node_2)
+        self.node_3 = BSTNode(3, 'node3', self.node_2)
+        self.node_5 = BSTNode(5, 'node5', self.node_6)
+        self.node_7 = BSTNode(7, 'node7', self.node_6)
+
+    def generate_bst(self):
+        self.node_4.LeftChild = self.node_2
+        self.node_4.RightChild = self.node_6
+        self.node_2.LeftChild = self.node_1
+        self.node_2.RightChild = self.node_3
+        self.node_6.LeftChild = self.node_5
+        self.node_6.RightChild = self.node_7
+
+        bst = BST(self.node_4)
+        return bst
+
+
+class BSTMocksGeneratorType3(BSTMocksGenerator):
+    """
+    Binary Strictly Tree object with following structure:
+                   8
+                 /    \
+               4       12
+             /  \     /  \
+            2    6    10   14
+           / \  / \  / \   / \
+          1  3  5 7  9 11 13 15
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.node_8 = BSTNode(8, 'node8', None)
+        self.node_4 = BSTNode(4, 'node4', self.node_8)
+        self.node_12 = BSTNode(12, 'node12', self.node_8)
+        self.node_2 = BSTNode(2, 'node2', self.node_4)
+        self.node_6 = BSTNode(6, 'node6', self.node_4)
+        self.node_1 = BSTNode(1, 'node1', self.node_2)
+        self.node_3 = BSTNode(3, 'node3', self.node_2)
+        self.node_5 = BSTNode(5, 'node5', self.node_6)
+        self.node_7 = BSTNode(7, 'node7', self.node_6)
+        self.node_10 = BSTNode(10, 'node10', self.node_12)
+        self.node_14 = BSTNode(14, 'node14', self.node_12)
+        self.node_9 = BSTNode(9, 'node9', self.node_10)
+        self.node_11 = BSTNode(11, 'node11', self.node_10)
+        self.node_13 = BSTNode(13, 'node13', self.node_14)
+        self.node_15 = BSTNode(15, 'node15', self.node_14)
+
+
+    def generate_bst(self):
+        self.node_8.LeftChild = self.node_4
+        self.node_8.RightChild = self.node_12
+        self.node_4.LeftChild = self.node_2
+        self.node_4.RightChild = self.node_6
+        self.node_2.LeftChild = self.node_1
+        self.node_2.RightChild = self.node_3
+        self.node_6.LeftChild = self.node_5
+        self.node_6.RightChild = self.node_7
+        self.node_12.LeftChild = self.node_10
+        self.node_12.RightChild = self.node_14
+        self.node_10.LeftChild = self.node_9
+        self.node_10.RightChild = self.node_11
+        self.node_14.LeftChild = self.node_13
+        self.node_14.RightChild = self.node_15
+
+        bst = BST(self.node_8)
+        return bst
+
+
+class TestFindNodeByKey(unittest.TestCase):
+    def test_find_node_by_key__1(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst_find = bst.FindNodeByKey(4)
+        result = (bst_find.Node, bst_find.NodeHasKey, bst_find.ToLeft)
+        reference = (bst_mocks_generator.node_3, False, False)
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__2(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst_find = bst.FindNodeByKey(2.5)
+        result = (bst_find.Node, bst_find.NodeHasKey, bst_find.ToLeft)
+        reference = (bst_mocks_generator.node_3, False, True)
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__3(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst_find = bst.FindNodeByKey(0.5)
+        result = (bst_find.Node, bst_find.NodeHasKey, bst_find.ToLeft)
+        reference = (bst_mocks_generator.node_1, False, True)
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__4(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst_find = bst.FindNodeByKey(1.5)
+        result = (bst_find.Node, bst_find.NodeHasKey, bst_find.ToLeft)
+        reference = (bst_mocks_generator.node_1, False, False)
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__5(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst_find = bst.FindNodeByKey(2)
+        result = (bst_find.Node, bst_find.NodeHasKey, bst_find.ToLeft)
+        reference = (bst_mocks_generator.node_2, True, False)
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__6(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst_find = bst.FindNodeByKey(1)
+        result = (bst_find.Node, bst_find.NodeHasKey, bst_find.ToLeft)
+        reference = (bst_mocks_generator.node_1, True, False)
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__7(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst_find = bst.FindNodeByKey(7)
+        result = (bst_find.Node, bst_find.NodeHasKey, bst_find.ToLeft)
+        reference = (bst_mocks_generator.node_7, True, False)
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__8(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst_find = bst.FindNodeByKey(1)
+        result = (bst_find.Node, bst_find.NodeHasKey, bst_find.ToLeft)
+        reference = (bst_mocks_generator.node_1, True, False)
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__9(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst_find = bst.FindNodeByKey(99)
+        result = (bst_find.Node, bst_find.NodeHasKey, bst_find.ToLeft)
+        reference = (bst_mocks_generator.node_7, False, False)
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__10(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst_find = bst.FindNodeByKey(2.5)
+        result = (bst_find.Node, bst_find.NodeHasKey, bst_find.ToLeft)
+        reference = (bst_mocks_generator.node_3, False, True)
+        self.assertEqual(result, reference)
+
+
+class TestAddKeyValue(unittest.TestCase):
+    def test_find_node_by_key__1(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.AddKeyValue(0.5, 'new_node')
+        result = bst_mocks_generator.node_1.LeftChild.NodeKey
+        reference = 0.5
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__2(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.AddKeyValue(99, 'new_node')
+        result = bst_mocks_generator.node_7.RightChild.NodeKey
+        reference = 99
+        self.assertEqual(result, reference)
+
+    def test_find_node_by_key__3(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.AddKeyValue(5, 'new_node')
+        self.assertFalse(result)
+
+
+class TestFinMinMax(unittest.TestCase):
+    def test_fin_min_max__1(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.FinMinMax(bst_mocks_generator.node_2, False)
+        reference = bst_mocks_generator.node_1
+        self.assertEqual(result, reference)
+
+    def test_fin_min_max__2(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.FinMinMax(bst_mocks_generator.node_2, True)
+        reference = bst_mocks_generator.node_3
+        self.assertEqual(result, reference)
+
+    def test_fin_min_max__3(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.FinMinMax(bst_mocks_generator.node_4, False)
+        reference = bst_mocks_generator.node_1
+        self.assertEqual(result, reference)
+
+    def test_fin_min_max__4(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.FinMinMax(bst_mocks_generator.node_4, True)
+        reference = bst_mocks_generator.node_7
+        self.assertEqual(result, reference)
+
+    def test_fin_min_max__5(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.FinMinMax(bst_mocks_generator.node_6, False)
+        reference = bst_mocks_generator.node_5
+        self.assertEqual(result, reference)
+
+    def test_fin_min_max__6(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.FinMinMax(bst_mocks_generator.node_2, True)
+        reference = bst_mocks_generator.node_3
+        self.assertEqual(result, reference)
+
+
+class TestCount(unittest.TestCase):
+    def test_count__1(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.Count()
+        self.assertEqual(result, 3)
+
+    def test_count__2(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.Count()
+        self.assertEqual(result, 7)
+
+
+class TestDeleteNodeToKey(unittest.TestCase):
+    def test_delete_node_to_key__1(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.DeleteNodeByKey(99)
+        self.assertEqual(result, False)
+
+    def test_delete_node_to_key__2(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        result = bst.DeleteNodeByKey(99)
+        self.assertEqual(result, False)
+
+    def test_delete_node_to_key__3(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(1)
+        self.assertEqual(bst_mocks_generator.node_2.LeftChild, None)
+
+    def test_delete_node_to_key__4(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(1)
+        self.assertEqual(bst_mocks_generator.node_2.RightChild, bst_mocks_generator.node_3)
+
+    def test_delete_node_to_key__5(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(2)
+        self.assertEqual(bst_mocks_generator.node_3.LeftChild, bst_mocks_generator.node_1)
+
+    def test_delete_node_to_key__6(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(2)
+        self.assertEqual(bst_mocks_generator.node_3.RightChild, None)
+
+    def test_delete_node_to_key__7(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(2)
+        self.assertEqual(bst_mocks_generator.node_3.Parent, None)
+
+    def test_delete_node_to_key__8(self):
+        bst_mocks_generator = BSTMocksGeneratorType1()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(2)
+        self.assertEqual(bst_mocks_generator.node_1.Parent, bst_mocks_generator.node_3)
+
+    def test_delete_node_to_key__9(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(7)
+        self.assertEqual(bst_mocks_generator.node_7.Parent, None)
+
+    def test_delete_node_to_key__10(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(7)
+        self.assertEqual(bst_mocks_generator.node_5.RightChild, None)
+
+    def test_delete_node_to_key__11(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(6)
+        self.assertEqual(bst_mocks_generator.node_4.RightChild, bst_mocks_generator.node_7)
+
+    def test_delete_node_to_key__12(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(6)
+        self.assertEqual(bst_mocks_generator.node_6.Parent, None)
+
+    def test_delete_node_to_key__13(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(4)
+        self.assertEqual(bst.Root, bst_mocks_generator.node_5)
+
+    def test_delete_node_to_key__14(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(4)
+        self.assertEqual(bst_mocks_generator.node_5.LeftChild, bst_mocks_generator.node_2)
+
+    def test_delete_node_to_key__15(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(4)
+        self.assertEqual(bst_mocks_generator.node_2.Parent, bst_mocks_generator.node_5)
+
+    def test_delete_node_to_key__16(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(4)
+        self.assertEqual(bst_mocks_generator.node_5.Parent, None)
+
+    def test_delete_node_to_key__17(self):
+        bst_mocks_generator = BSTMocksGeneratorType2()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(4)
+        self.assertEqual(bst_mocks_generator.node_5.Parent, None)
+
+    def test_delete_node_to_key__18(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(4)
+        self.assertEqual(bst_mocks_generator.node_8.LeftChild, bst_mocks_generator.node_5)
+
+    def test_delete_node_to_key__19(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(4)
+        self.assertEqual(bst_mocks_generator.node_5.Parent, bst_mocks_generator.node_8)
+
+    def test_delete_node_to_key__20(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(4)
+        self.assertEqual(bst_mocks_generator.node_5.LeftChild, bst_mocks_generator.node_2)
+
+    def test_delete_node_to_key__21(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(4)
+        self.assertEqual(bst_mocks_generator.node_5.RightChild, bst_mocks_generator.node_6)
+
+    def test_delete_node_to_key__22(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(4)
+        self.assertEqual(bst_mocks_generator.node_6.LeftChild, None)
+
+    def test_delete_node_to_key__23(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(12)
+        self.assertEqual(bst_mocks_generator.node_8.RightChild, bst_mocks_generator.node_13)
+
+    def test_delete_node_to_key__24(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(12)
+        self.assertEqual(bst_mocks_generator.node_13.Parent, bst_mocks_generator.node_8)
+
+    def test_delete_node_to_key__25(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(12)
+        self.assertEqual(bst_mocks_generator.node_13.LeftChild, bst_mocks_generator.node_10)
+
+    def test_delete_node_to_key__26(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(12)
+        print(bst_mocks_generator.node_13.LeftChild.NodeKey)
+        self.assertEqual(bst_mocks_generator.node_13.RightChild, bst_mocks_generator.node_14)
+
+    def test_delete_node_to_key__27(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(8)
+        self.assertEqual(bst.Root, bst_mocks_generator.node_9)
+
+    def test_delete_node_to_key__28(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(8)
+        self.assertEqual(bst_mocks_generator.node_9.Parent, None)
+
+    def test_delete_node_to_key__29(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(8)
+        self.assertEqual(bst_mocks_generator.node_9.LeftChild, bst_mocks_generator.node_4)
+
+    def test_delete_node_to_key__30(self):
+        bst_mocks_generator = BSTMocksGeneratorType3()
+        bst = bst_mocks_generator.generate_bst()
+
+        bst.DeleteNodeByKey(8)
+        self.assertEqual(bst_mocks_generator.node_4.Parent, bst_mocks_generator.node_9)
+
+
+if __name__ == '__main__':
+    unittest.main()
