8ca20a5a9f2c1fc7df3c944474fa38e4cb2e0a05
Nikita B <n13b@inbox.ru>
2023-08-04 22:12:22 +0600

    Added twelve task (Finding weak vertices in a graph) with unittests (50+)

---
+++ course_1_3/Task_12.py
@@ -0,0 +1,94 @@
+from collections import deque
+from itertools import combinations
+
+class Vertex:
+    def __init__(self, val):
+        self.Value = val
+        self.Hit = False
+
+
+class SimpleGraph:
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+
+    def AddVertex(self, v) -> None:
+        vert_obj = Vertex(v)
+        for i, el in enumerate(self.vertex):
+            if el is None:
+                self.vertex[i] = vert_obj
+                return
+
+    def RemoveVertex(self, v) -> None:
+        self.vertex[v] = None
+        self.m_adjacency[v] = [0] * len(self.vertex)
+        for i in self.m_adjacency:
+            i[v] = 0
+
+    def IsEdge(self, v1, v2) -> bool:
+        return bool(self.m_adjacency[v1][v2])
+
+    def AddEdge(self, v1, v2) -> None:
+        self.m_adjacency[v1][v2] = 1
+        self.m_adjacency[v2][v1] = 1
+
+    def RemoveEdge(self, v1, v2) -> None:
+        self.m_adjacency[v1][v2] = 0
+        self.m_adjacency[v2][v1] = 0
+
+    def BreadthFirstSearch(self, VFrom, VTo):
+        self.__mark_all_vertices_as_unvisited()
+        if self.vertex[VFrom] is None or self.vertex[VTo] is None:
+            return []
+        path_to_vertex = []
+        init_deq = deque(
+            [[VFrom, path_to_vertex]]
+        )  # create dataclass would be much better
+        return self.__breadth_first_search(VTo, init_deq)
+
+    def __mark_all_vertices_as_unvisited(self):
+        for i in self.vertex:
+            if i:
+                i.Hit = False
+
+    def __breadth_first_search(self, v_to: int, deq: deque) -> list:
+        if not deq:
+            return []
+        vertex = deq.pop()
+        vertex_ind = vertex[0]
+        vertex_path = vertex[1]
+        vertex_adj_ind = self.m_adjacency[vertex_ind]
+        vertex_obj = self.vertex[vertex_ind]
+        v_to_vertex = self.vertex[v_to]
+        if vertex_obj is v_to_vertex:
+            return vertex_path + [vertex_obj]
+        vertex_obj.Hit = True
+        adjacent_vertexes = [i for i in range(self.max_vertex) if vertex_adj_ind[i]]
+        for i in adjacent_vertexes:
+            if self.vertex[i].Hit is not True:
+                vertex_path_i = vertex_path + [vertex_obj]
+                deq.appendleft([i, vertex_path_i])
+        return self.__breadth_first_search(v_to, deq)
+
+    def WeakVertices(self):
+        weak_vertices = []
+        for i, node in enumerate(self.vertex):
+            if not node:
+                continue
+            if self.__is_vertice_weak(i):
+                weak_vertices.append(node)
+        return weak_vertices
+
+    def __is_vertice_weak(self, v: int) -> bool:
+        adjacency = []
+        for j, elem in enumerate(self.m_adjacency[v]):
+            if j == v:
+                continue
+            if elem == 1:
+                adjacency.append(j)
+        adjacency_combinations = combinations(adjacency, 2)
+        for row, column in adjacency_combinations:
+            if self.m_adjacency[row][column]:
+                return False
+        return True
---
+++ course_1_3/tests/test_Task_12.py
@@ -0,0 +1,613 @@
+from ..Task_12 import *
+import unittest
+
+
+class GraphMocksGenerator:
+    def __init__(self):
+        pass
+
+    def generate_graph(self) -> SimpleGraph:
+        pass
+
+
+class GraphMocksGeneratorType0(GraphMocksGenerator):
+    """
+    Сonnected undirected graph object with following structure:
+        0 - 2
+           /
+          1
+    """
+
+    def generate_graph(self) -> SimpleGraph:
+        graph = SimpleGraph(5)
+        graph.AddVertex(0)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddEdge(0, 2)
+        graph.AddEdge(2, 1)
+        return graph
+
+
+class GraphMocksGeneratorType1(GraphMocksGenerator):
+    """
+    Сonnected undirected graph object with following structure:
+        0 - 2
+           /
+          1 - 3
+          |
+          4
+    """
+
+    def generate_graph(self) -> SimpleGraph:
+        graph = SimpleGraph(10)
+        graph.AddVertex(0)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddVertex(4)
+
+        graph.AddEdge(0, 2)
+        graph.AddEdge(2, 1)
+        graph.AddEdge(1, 3)
+        graph.AddEdge(1, 4)
+        return graph
+
+
+class GraphMocksGeneratorType2(GraphMocksGenerator):
+    """
+    Сonnected undirected graph object with following structure:
+                0
+               / \
+              4   2
+              |   |
+              1 - 3
+    """
+
+    def generate_graph(self) -> SimpleGraph:
+        graph = SimpleGraph(10)
+        graph.AddVertex(0)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddVertex(4)
+
+        graph.AddEdge(0, 2)
+        graph.AddEdge(2, 3)
+        graph.AddEdge(3, 1)
+        graph.AddEdge(1, 4)
+        graph.AddEdge(4, 0)
+        return graph
+
+
+class GraphMocksGeneratorType3(GraphMocksGenerator):
+    """
+    Сonnected undirected graph object with following structure:
+                0 - 1 - 4
+                |     /
+            7 - 2 - 3
+            |   |  /
+            6 - 5
+    """
+
+    def generate_graph(self) -> SimpleGraph:
+        graph = SimpleGraph(10)
+        graph.AddVertex(0)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddVertex(4)
+        graph.AddVertex(5)
+        graph.AddVertex(6)
+        graph.AddVertex(7)
+
+        graph.AddEdge(0, 1)
+        graph.AddEdge(0, 2)
+        graph.AddEdge(1, 4)
+        graph.AddEdge(2, 3)
+        graph.AddEdge(2, 5)
+        graph.AddEdge(2, 7)
+        graph.AddEdge(3, 4)
+        graph.AddEdge(3, 5)
+        graph.AddEdge(5, 6)
+        graph.AddEdge(6, 7)
+        return graph
+
+
+class GraphMocksGeneratorType4(GraphMocksGenerator):
+    """
+    Сonnected undirected graph object with following structure:
+                0 - 1 - 4 - 5
+                | \ |  /
+                2 - 3
+                   /\
+                   ︺
+    """
+
+    def generate_graph(self) -> SimpleGraph:
+        graph = SimpleGraph(6)
+        graph.AddVertex(0)  # A
+        graph.AddVertex(1)  # B
+        graph.AddVertex(2)  # C
+        graph.AddVertex(3)  # D
+        graph.AddVertex(4)  # E
+        graph.AddVertex(5)  # F
+
+        graph.AddEdge(0, 1)
+        graph.AddEdge(0, 2)
+        graph.AddEdge(0, 3)
+        graph.AddEdge(1, 3)
+        graph.AddEdge(1, 4)
+        graph.AddEdge(2, 3)
+        graph.AddEdge(3, 3)
+        graph.AddEdge(3, 4)
+        graph.AddEdge(4, 5)
+
+        return graph
+
+
+class GraphMocksGeneratorType5(GraphMocksGenerator):
+    """
+    Сonnected undirected graph object with following structure:
+            0 --- 4   6
+           / \    | / |
+          1 - 2 - 5 - 7
+          \  /        |
+           3          8
+    """
+
+    def generate_graph(self) -> SimpleGraph:
+        graph = SimpleGraph(10)
+        graph.AddVertex(0)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddVertex(4)
+        graph.AddVertex(5)
+        graph.AddVertex(6)
+        graph.AddVertex(7)
+        graph.AddVertex(8)
+
+        graph.AddEdge(0, 1)
+        graph.AddEdge(0, 2)
+        graph.AddEdge(0, 4)
+        graph.AddEdge(1, 2)
+        graph.AddEdge(1, 3)
+        graph.AddEdge(2, 3)
+        graph.AddEdge(2, 5)
+        graph.AddEdge(4, 5)
+        graph.AddEdge(5, 6)
+        graph.AddEdge(5, 7)
+        graph.AddEdge(6, 7)
+        graph.AddEdge(7, 8)
+
+        return graph
+
+
+class TestAddVertex(unittest.TestCase):
+    def test_add_vertex__1(self):
+        graph = SimpleGraph(0)
+        graph.AddVertex(99)
+        self.assertEqual(graph.vertex, [])
+
+    def test_add_vertex__2(self):
+        graph = SimpleGraph(1)
+        self.assertEqual(graph.vertex, [None])
+
+    def test_add_vertex__3(self):
+        graph = SimpleGraph(1)
+        graph.AddVertex(99)
+        result = [i.Value for i in graph.vertex]
+        reference = [99]
+        self.assertEqual(reference, result)
+
+    def test_add_vertex__4(self):
+        graph = SimpleGraph(2)
+        graph.AddVertex(99)
+        result = [i.Value if i else i for i in graph.vertex]
+        reference = [99, None]
+        self.assertEqual(reference, result)
+
+    def test_add_vertex__5(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(4)
+        graph.AddVertex(1)
+        graph.AddVertex(5)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        result = [i.Value if i else i for i in graph.vertex]
+        reference = [4, 1, 5, 2, 3]
+        self.assertEqual(reference, result)
+
+    def test_add_vertex__6(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(4)
+        graph.AddVertex(1)
+        graph.AddVertex(5)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddVertex(99)
+        result = [i.Value if i else i for i in graph.vertex]
+        reference = [4, 1, 5, 2, 3]
+        self.assertEqual(reference, result)
+
+    def test_add_vertex__7(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(4)
+        graph.AddVertex(1)
+        graph.AddVertex(5)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.RemoveVertex(0)
+        graph.AddVertex(99)
+        result = [i.Value if i else i for i in graph.vertex]
+        reference = [99, 1, 5, 2, 3]
+        self.assertEqual(reference, result)
+
+    def test_add_vertex__8(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(4)
+        graph.AddVertex(1)
+        graph.AddVertex(5)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.RemoveVertex(4)
+        graph.RemoveVertex(1)
+        graph.AddVertex(99)
+        graph.AddVertex(66)
+        result = [i.Value if i else i for i in graph.vertex]
+        reference = [4, 99, 5, 2, 66]
+        self.assertEqual(reference, result)
+
+
+class TestRemoveVertex(unittest.TestCase):
+    def test_remove_vertex__1(self):
+        graph = SimpleGraph(5)
+        graph.RemoveVertex(4)
+        result = [i.Value if i else i for i in graph.vertex]
+        reference = [None, None, None, None, None]
+        self.assertEqual(reference, result)
+
+    def test_remove_vertex__2(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(99)
+        graph.RemoveVertex(0)
+        result = [i.Value if i else i for i in graph.vertex]
+        reference = [None, None, None, None, None]
+        self.assertEqual(reference, result)
+
+    def test_remove_vertex__3(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.RemoveVertex(0)
+        result = [i.Value if i else i for i in graph.vertex]
+        reference = [None, 2, None, None, None]
+        self.assertEqual(reference, result)
+
+    def test_remove_vertex__4(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddEdge(0, 1)
+        graph.RemoveVertex(1)
+        result = graph.m_adjacency[0][1]
+        reference = 0
+        self.assertEqual(reference, result)
+
+    def test_remove_vertex__5(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddEdge(0, 1)
+        graph.RemoveVertex(1)
+        result = graph.m_adjacency[1][0]
+        reference = 0
+        self.assertEqual(reference, result)
+
+
+class TestIsEdge(unittest.TestCase):
+    def test_is_edge__1(self):
+        graph = SimpleGraph(5)
+        result = graph.IsEdge(0, 3)
+        self.assertFalse(result)
+
+    def test_is_edge__2(self):
+        graph = SimpleGraph(5)
+        result = graph.IsEdge(0, 0)
+        self.assertFalse(result)
+
+    def test_is_edge__3(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddEdge(0, 0)
+        result = graph.IsEdge(0, 0)
+        self.assertTrue(result)
+
+    def test_is_edge__4(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddEdge(0, 2)
+        result = graph.IsEdge(2, 0)
+        self.assertTrue(result)
+
+    def test_is_edge__5(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddEdge(1, 2)
+        result = graph.IsEdge(1, 2)
+        self.assertTrue(result)
+
+    def test_is_edge__6(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddEdge(1, 2)
+        result = graph.IsEdge(2, 1)
+        self.assertTrue(result)
+
+
+class TestAddEdge(unittest.TestCase):
+    def test_add_edge__1(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddEdge(0, 1)
+        result = graph.m_adjacency[0][1]
+        self.assertEqual(1, result)
+
+    def test_add_edge__2(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddEdge(0, 1)
+        result = graph.m_adjacency[1][0]
+        self.assertEqual(1, result)
+
+    def test_add_edge__3(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddEdge(0, 2)
+        graph.AddEdge(2, 1)
+        result = graph.m_adjacency[1][2]
+        self.assertEqual(1, result)
+
+    def test_add_edge__4(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddEdge(0, 2)
+        graph.AddEdge(2, 1)
+        result = graph.m_adjacency[2][1]
+        self.assertEqual(1, result)
+
+
+class TestRemoveEdge(unittest.TestCase):
+    def test_remove_edge__1(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddEdge(0, 2)
+        graph.AddEdge(2, 1)
+        graph.RemoveEdge(0, 2)
+        result = graph.m_adjacency[0][2]
+        self.assertEqual(0, result)
+
+    def test_remove_edge__2(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddEdge(0, 2)
+        graph.AddEdge(2, 1)
+        graph.RemoveEdge(0, 2)
+        result = graph.m_adjacency[2][0]
+        self.assertEqual(0, result)
+
+    def test_remove_edge__3(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddEdge(0, 2)
+        graph.AddEdge(2, 1)
+        graph.RemoveEdge(0, 2)
+        graph.RemoveEdge(1, 2)
+        result = graph.m_adjacency[1][2]
+        self.assertEqual(0, result)
+
+    def test_remove_edge__4(self):
+        graph = SimpleGraph(5)
+        graph.AddVertex(1)
+        graph.AddVertex(2)
+        graph.AddVertex(3)
+        graph.AddEdge(0, 2)
+        graph.AddEdge(2, 1)
+        graph.RemoveEdge(0, 2)
+        graph.RemoveEdge(1, 2)
+        result = graph.m_adjacency[2][1]
+        self.assertEqual(0, result)
+
+
+class TestBreadthFirstSearch(unittest.TestCase):
+    def test_breadth_first_search__1(self):
+        graph_mocks_generator = GraphMocksGeneratorType0()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(1, 2)]
+        self.assertEqual(result, [1, 2])
+
+    def test_breadth_first_search__2(self):
+        graph_mocks_generator = GraphMocksGeneratorType0()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(0, 1)]
+        self.assertEqual(result, [0, 2, 1])
+
+    def test_breadth_first_search__3(self):
+        graph_mocks_generator = GraphMocksGeneratorType0()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(1, 0)]
+        self.assertEqual(result, [1, 2, 0])
+
+    def test_breadth_first_search__4(self):
+        graph_mocks_generator = GraphMocksGeneratorType0()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(4, 3)]
+        self.assertEqual(result, [])
+
+    def test_breadth_first_search__5(self):
+        graph_mocks_generator = GraphMocksGeneratorType0()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(1, 2)]
+        self.assertEqual(result, [1, 2])
+
+    def test_breadth_first_search__6(self):
+        graph_mocks_generator = GraphMocksGeneratorType0()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(1, 1)]
+        self.assertEqual(result, [1])
+
+    def test_breadth_first_search__7(self):
+        graph_mocks_generator = GraphMocksGeneratorType1()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(0, 4)]
+        self.assertEqual(result, [0, 2, 1, 4])
+
+    def test_breadth_first_search__8(self):
+        graph_mocks_generator = GraphMocksGeneratorType1()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(3, 4)]
+        self.assertEqual(result, [3, 1, 4])
+
+    def test_breadth_first_search__9(self):
+        graph_mocks_generator = GraphMocksGeneratorType1()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(1, 3)]
+        self.assertEqual(result, [1, 3])
+
+    def test_breadth_first_search__10(self):
+        graph_mocks_generator = GraphMocksGeneratorType1()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(1, 3)]
+        self.assertEqual(result, [1, 3])
+
+    def test_breadth_first_search__11(self):
+        graph_mocks_generator = GraphMocksGeneratorType2()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(0, 3)]
+        self.assertEqual(result, [0, 2, 3])
+
+    def test_breadth_first_search__12(self):
+        graph_mocks_generator = GraphMocksGeneratorType2()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(0, 4)]
+        self.assertEqual(result, [0, 4])
+
+    def test_breadth_first_search__13(self):
+        graph_mocks_generator = GraphMocksGeneratorType2()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(0, 3)]
+        self.assertEqual(result, [0, 2, 3])
+
+    def test_breadth_first_search__14(self):
+        graph_mocks_generator = GraphMocksGeneratorType2()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(0, 3)]
+        self.assertEqual(result, [0, 2, 3])
+
+    def test_breadth_first_search__15(self):
+        graph_mocks_generator = GraphMocksGeneratorType2()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(0, 1)]
+        self.assertEqual(result, [0, 4, 1])
+
+    def test_breadth_first_search__16(self):
+        """
+        Reproducing
+        <Error or failed test:
+        An existing path from a single edge is not found>
+
+        On the graph with the following structure:
+                    0 - 1 - 4
+                    |     /
+                7 - 2 - 3
+                |   |  /
+                6 - 5
+        """
+        graph_mocks_generator = GraphMocksGeneratorType3()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.BreadthFirstSearch(7, 6)]
+        self.assertEqual(result, [7, 6])
+
+    def test_breadth_first_search__17(self):
+        """
+        Reproducing
+        <Error or failed test:
+        An existing path from a single edge is not found>
+
+        On the graph with the following structure:
+                0 - 1 - 4 - 5
+                | \ |  /
+                2 - 3
+                   /\
+                   ︺
+        """
+        graph_mocks_generator = GraphMocksGeneratorType4()
+        graph = graph_mocks_generator.generate_graph()
+        graph.BreadthFirstSearch(0, 4)
+        result = [i.Value for i in graph.BreadthFirstSearch(3, 0)]
+        self.assertEqual(result, [3, 0])
+
+
+class TestWeakVertices(unittest.TestCase):
+    def test_weak_vertices__1(self):
+        graph_mocks_generator = GraphMocksGeneratorType4()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.WeakVertices()]
+        self.assertEqual(result, [5])
+
+    def test_weak_vertices__2(self):
+        graph_mocks_generator = GraphMocksGeneratorType5()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.WeakVertices()]
+        self.assertEqual(result, [4, 8])
+
+    def test_weak_vertices__3(self):
+        graph_mocks_generator = GraphMocksGeneratorType3()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.WeakVertices()]
+        self.assertEqual(result, [0, 1, 4, 6, 7])
+
+    def test_weak_vertices__4(self):
+        graph_mocks_generator = GraphMocksGeneratorType2()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.WeakVertices()]
+        self.assertEqual(result, [0, 1, 2, 3, 4])
+
+    def test_weak_vertices__5(self):
+        graph_mocks_generator = GraphMocksGeneratorType1()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.WeakVertices()]
+        self.assertEqual(result, [0, 1, 2, 3, 4])
+
+    def test_weak_vertices__6(self):
+        graph_mocks_generator = GraphMocksGeneratorType0()
+        graph = graph_mocks_generator.generate_graph()
+        result = [i.Value for i in graph.WeakVertices()]
+        self.assertEqual(result, [0, 1, 2])
+
+    def test_weak_vertices__7(self):
+        graph_mocks_generator = GraphMocksGeneratorType5()
+        graph = graph_mocks_generator.generate_graph()
+        graph.WeakVertices()
+        result = [i.Value for i in graph.WeakVertices()]
+        self.assertEqual(result, [4, 8])
