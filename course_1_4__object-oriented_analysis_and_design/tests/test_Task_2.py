770fcaa762a790c4b3b6a6e61dfe539d6e5b5732
MaxFallishe <n13b@inbox.ru>
2024-03-17 00:30:56 +0300

    Added second task of object-oriented analysis and design (Design LinkedList ADT) with only unittests templates

---
+++ course_1_4__object-oriented_analysis_and_design/tests/test_Task_2.py
@@ -0,0 +1,42 @@
+import unittest
+from ..Task_2__design_ADT_for_linked_list import LinkedList
+
+
+class TestHeadOperation(unittest.TestCase):
+    pass
+
+
+class TestTailOperation(unittest.TestCase):
+    pass
+
+
+class TestRightOperation(unittest.TestCase):
+    pass
+
+
+class TestGetOperation(unittest.TestCase):
+    pass
+
+
+class TestPutRightOperation(unittest.TestCase):
+    pass
+
+
+class TestPutLeftOperation(unittest.TestCase):
+    pass
+
+
+class TestRemoveOperation(unittest.TestCase):
+    pass
+
+
+class TestClearOperation(unittest.TestCase):
+    pass
+
+
+class TestSizeOperation(unittest.TestCase):
+    pass
+
+
+class TestAddToEmptyOperation(unittest.TestCase):
+    pass
