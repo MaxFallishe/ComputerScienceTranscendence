006da00c3b797b843f0503134b4e9c25cb43f989
MaxFallishe <n13b@inbox.ru>
2024-11-26 17:27:07 +0300

    Added first task of python-functional-programming (A monad is a monoid in the category of endofunctors)

---
+++ course_1_6__python_functional_programming/Task_1__.py
@@ -0,0 +1,22 @@
+# 1. Пример проекта из личной практики в котором можно было бы избавиться от хранения промежуточных  результатов и
+# использования промежуточных результатов:
+#
+#
+#
+# 2. Измышления о том, как в принципе может быть устроена архитектура сложной системы, где промежуточные состояния
+# вычислений вообще никак не хранятся в явном виде (за исключением каких-то специфичных настроек):
+#
+#
+#
+
+
+# 1. An example of a project from personal practice in which it would be possible to get rid of storing intermediate
+# results and using intermediate results:
+#
+#
+#
+# 2. Inventions about how, in principle, the architecture of a complex system can be arranged, where intermediate states
+# calculations are not explicitly stored in any way (except for some specific settings):
+#
+#
+#
