a5a9fd8b80101aa1a9dbceacb954015789e82d4e
MaxFallishe <n13b@inbox.ru>
2024-12-10 22:11:26 +0300

    Added tests for seventh task of python-functional-programming (FP little practice)

--- course_1_6__python_functional_programming/tests/test_Task_7__functional_programming_practice.py
+++ course_1_6__python_functional_programming/tests/test_Task_7__functional_programming_practice.py
@@ -14,6 +14,14 @@ class TestConquestCampign(unittest.TestCase):
     ⚫ - conquered cell
     """
 
+    def test_1x2_field_with_single_start_coordinate(self):
+        """
+        Stage №1  -> Stage №2
+        ⚫ ⚪         ⚫ ⚫
+        """
+        result = ConquestCampaign(N=1, M=2, L=1, batallion=Just([1, 1]))
+        assert result == 2
+
     def test_2x2_field_with_single_start_coordinate(self):
         """
         Stage №1  -> Stage №2  -> Stage №3
@@ -32,3 +40,16 @@ class TestConquestCampign(unittest.TestCase):
         """
         result = ConquestCampaign(N=3, M=4, L=2, batallion=Just([2, 2, 3, 4]))
         assert result == 3
+
+    def test_2x6_field_with_one_start_coordinate(self):
+        """
+        Stage №1 -> Stage №2 -> Stage №3 -> Stage №4 -> Stage №5 -> Stage №6
+        ⚪ ⚪          ⚪ ⚫       ⚫ ⚫        ⚫ ⚫        ⚫ ⚫        ⚫ ⚫
+        ⚪ ⚫          ⚫ ⚫       ⚫ ⚫        ⚫ ⚫        ⚫ ⚫        ⚫ ⚫
+        ⚪ ⚪          ⚪ ⚫       ⚫ ⚫        ⚫ ⚫        ⚫ ⚫        ⚫ ⚫
+        ⚪ ⚪          ⚪ ⚪       ⚪ ⚫        ⚫ ⚫        ⚫ ⚫        ⚫ ⚫
+        ⚪ ⚪          ⚪ ⚪       ⚪ ⚪        ⚪ ⚫        ⚫ ⚫        ⚫ ⚫
+        ⚪ ⚪          ⚪ ⚪       ⚪ ⚪        ⚪ ⚪        ⚪ ⚫        ⚫ ⚫
+        """
+        result = ConquestCampaign(N=2, M=6, L=1, batallion=Just([2, 2]))
+        assert result == 6
