1be250610e1a072efedecf3775e8c41a7ea7aafd
MaxFallishe <n13b@inbox.ru>
2025-01-21 12:53:15 +0300

    Edited ninth task of F# practice (tuples and operators)
    Added another realization of rpgMoney calculator and optimization of complex numbers operations

--- course_1_7__fsharp_practice/Task_9__tuples_and_operators.fs
+++ course_1_7__fsharp_practice/Task_9__tuples_and_operators.fs
@@ -8,37 +8,30 @@ let rpgMoney (goldCoins,silverCoins, bronzeCoins) =
         bronzeCoins % bronze2silverCoinsRate
     )
 
-let bronzeMoney (goldCoins,silverCoins, bronzeCoins) =
-    (
-        0,
-        0,
-        bronzeCoins + (bronze2silverCoinsRate * silverCoins) + (silver2goldCoinsRate * bronze2silverCoinsRate * goldCoins)
-    )
-
-let rpgMoneyAdd ((a, b, c), (e, f, g)) =
-    rpgMoney (a + e, b + f, c + g)
-
-let rpgMoneySubtraction ((a, b, c), (e, f, g)) =
-    rpgMoney (a - e, b - f, c - g)
-
-let complexNumbersAdd((a, b), (c, d)) =  (a + c, b + d)
-
-let complexNumbersSubtraction((a, b), (c, d)) =  (a - c, b - d)
-
-let complexNumbersMultiplication ((a: float, b: float), (c: float, d: float)) =
-    (a * c - b * d, b * c + a * d)
-
-let complexNumbersDivide((a, b), (c, d)) =
-    let denominator = c ** 2.0 + d ** 2.0
-    complexNumbersMultiplication ((a, b), (c / denominator, -d / denominator))
-
 
 // 23.4.1
-let (.+.) x y = rpgMoneyAdd(x, y)
-let (.-.) x y = rpgMoneySubtraction(bronzeMoney(x), bronzeMoney(y))
+let (.+.) (a, b, c) (e, f, g)= rpgMoney (a + e, b + f, c + g)
+let (.-.) (a, b, c) (e, f, g) = rpgMoney(
+    0,
+    0,
+    c + (bronze2silverCoinsRate * b) + (silver2goldCoinsRate * bronze2silverCoinsRate * a)
+    - (g + (bronze2silverCoinsRate * f) + (silver2goldCoinsRate * bronze2silverCoinsRate * e))
+)
 
 // 23.4.2
-let (.+) x y = complexNumbersAdd(x, y)
-let (.-) x y = complexNumbersSubtraction(x, y)
-let (.*) x y = complexNumbersMultiplication(x, y)
-let (./) x y = complexNumbersDivide(x, y)
+let (.+) (a, b) (c, d) = (a + c, b + d)
+let (.-) (a, b) (c, d) = (a-c, b-d)
+let (.*) (a, b) (c, d) = (a * c - b * d, b * c + a * d)
+let (./) (a, b) (c, d) = (a, b) .* (c / (c * c + d * d), -d / (c * c + d * d))
+
+// Another reealization for rpgMoney calculator
+// let rec iter = function
+//     | (a, b, c) when c > 11 -> iter (a, b + (c / 12), c % 12)
+//     | (a, b, c) when b > 19 -> iter (a + (b / 20), b % 20, c)
+//     | (a, b, c) when c < 0 && b > 0 -> iter (a, b - 1, c + 12)
+//     | (a, b, c) when b < 0 && a > 0 -> iter (a - 1, b + 20, c)
+//     | (a, b, c) when c < 0 && a > 0 -> iter (a - 1, b + 19, c + 12)
+//     | (a, b, c) -> (a, b, c)
+//
+// let (.+.) (a, b, c) (x, y, z) = iter (a + x, b + y, c + z)
+// let (.-.) (a, b, c) (x, y, z) = iter (a - x, b - y, c - z)
