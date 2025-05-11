f0df359fffbf6bb11d9b4ab5555ec2944fcd1888
MaxFallishe <n13b@inbox.ru>
2025-05-11 23:52:12 +0300

    Added first task of SMT Solver Z3 course (Computational Notabooks)

---
+++ course_1_9__smt_solver_z3/Task_1__computational_notebooks.py
@@ -0,0 +1,37 @@
+# RU
+# Интерактивные ноутбуки по типу Jupyter Notebook и Google Colab являются достаточно нишевыми инструментами с точки
+# зрения среднего бэкенд веб-разработчика, однако их объективную полезность и применимость не стоит отрицать.
+# Подобные инструменты в первую очередь активно используются в областях
+# напрямую не связанных с прикладной разработкой веб-решений, к примеру - анализ данных, работа с моделями машинного
+# обучения и более продвинутое глубокое обучение. Отчего, не всегда даже python-разработчик будет знаком с подобными инструментами.
+# В то время как перечисленные выше области примения интерактивных ноутбуков чаще всего подразумевают написание программного
+# кода именно на python.
+# Основная сложность применения интерактивных ноутбуков, скорее связана со сложностью их локальной
+# настройки в первый раз, нежели их концептуальной сложности. Однако данный тезис касается скорее решений по типу Jupyter Notebook
+# которые подразумевают, что скорее всего пользователю будет необходимо самому локально развернуть решение, позволяющее
+# работать в парадигме интерактивного ноутбука. Поэтому Google Colab будет скорее более дружелюбен для нового пользователя,
+# однако самостоятельная локальная настройка Jupyter Notebook является хорошим опытом преодоления проблем связанных с
+# несовместимостью зависимостей, пакетными менеджарами и оптимизацией решений под конкретное hardware.
+# В частности, хорошая практика, при локальном использовании интерактивных ноутбуков - создать отдельное виртуальное python
+# окружение в рамках которого будут производится все эксперементы с наукоёмким кодом, также стоит сразу установить пакетный
+# менеджер `conda`.
+
+
+# EN
+# Interactive notebooks like Jupyter Notebook and Google Colab are quite niche tools in terms of
+# From the point of view of the average backend web developer, however, their objective usefulness and applicability should not be denied. 
+# Such tools are primarily actively used in the fields of
+# web solutions that are not directly related to applied development, for example, data analysis, working with machine learning models
+# learning and more advanced deep learning. Why, even a python developer won't always be familiar with such tools.
+# While the areas listed above for using interactive notebooks most often involve writing software
+# the code is in python.
+# The main difficulty of using interactive notebooks is rather related to the complexity of their local
+# settings for the first time, rather than their conceptual complexity. However, this thesis is more about Jupyter Notebook type solutions.
+# Which implies that most likely the user will need to deploy a solution locally himself, allowing
+# work in the interactive notebook paradigm. Therefore, Google Colab will be more friendly to the new user.,
+# however, setting up Jupyter Notebook yourself locally is a good experience to overcome the problems associated with
+# incompatibility of dependencies, package managers, and optimization of solutions for specific hardware.
+# In particular, a good practice when using interactive notebooks locally is to create a separate virtual python
+# the environment in which all experiments with high-tech code will be performed, it is also worth installing a batch one right away
+# manager of `conda'.
+
