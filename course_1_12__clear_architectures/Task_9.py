6ffe9fd6a1325113d71bc71e6fd3933e2fc324de
MaxFallishe <n13b@inbox.ru>
2026-05-05 21:56:41 +0300

    Added ninth task of Clean Architecture course (Functional Dependency Injection 2)

---
+++ course_1_12__clear_architectures/Task_9.py
@@ -0,0 +1,49 @@
+import pure_robot
+from types import ModuleType
+
+
+class RobotApi:
+
+    def setup(self, robot_funcs: ModuleType):
+        self.funcs = robot_funcs
+
+    def make(self, command):
+        if not hasattr(self, 'cleaner_state'):
+            self.cleaner_state = pure_robot.RobotState(0.0, 0.0, 0, pure_robot.WATER)
+
+        cmd = command.split(' ')
+        if cmd[0] == 'move':
+            self.cleaner_state = self.funcs.move(self.funcs.transfer_to_cleaner, int(cmd[1]), self.cleaner_state)
+        elif cmd[0] == 'turn':
+            self.cleaner_state = self.funcs.turn(self.funcs.transfer_to_cleaner, int(cmd[1]), self.cleaner_state)
+        elif cmd[0] == 'set':
+            self.cleaner_state = self.funcs.set_state(self.funcs.transfer_to_cleaner, cmd[1], self.cleaner_state)
+        elif cmd[0] == 'start':
+            self.cleaner_state = self.funcs.start(self.funcs.transfer_to_cleaner, self.cleaner_state)
+        elif cmd[0] == 'stop':
+            self.cleaner_state = self.funcs.stop(self.funcs.transfer_to_cleaner, self.cleaner_state)
+        return self.cleaner_state
+
+    def __call__(self, command):
+        return self.make(command)
+
+
+def transfer_to_cleaner_to_cleaner(message):
+    print(message)
+
+
+def double_move(transfer_to_cleaner, dist, state):
+    return pure_robot.move(transfer_to_cleaner, dist * 2, state)
+
+
+api = RobotApi()
+api.setup(pure_robot)
+
+# from cleaner_api import api
+
+api('move 100')
+api('turn -90')
+api('set soap')
+api('start')
+api('move 50')
+s = api('stop')
