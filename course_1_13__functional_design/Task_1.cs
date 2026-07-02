478d627d4946abfa2283787d46a93daedd3cb5bd
MaxFallishe <n13b@inbox.ru>
2026-07-02 23:44:47 +0300

    Added first task of Functional Design (Initialize Game)

---
+++ course_1_13__functional_design/Task_1.cs
@@ -0,0 +1,11 @@
+// Logic plan for the implementation of random filling of the game board
+// Step 1. Select as a parameter of the filling strategy a function that implements random filling, for example RandomBoardFillStrategy(...)
+// Step 2. The RandomBoardFillStrategy function(...) receives the Board structure as input and reads the size of the playing field + other characteristics if necessary
+// Step 3. Inside RandomBoardFillStrategy(...) for each cell of the playing field, its value is calculated.
+// Step 4. According to the generated values, a new Board structure is built and returned with updated values for the cells of the playing field.
+
+// План логики реализации случайного заполнения игровой доски
+// Шаг 1. Выбираем в качестве параметра стратегии заполнения - функцию реализующую случайное заполнение, например RandomBoardFillStrategy(...)
+// Шаг 2. Функция RandomBoardFillStrategy(...) получает на вход структуру Board и считывает размер игрового поля + другие характеристики при необходимости
+// Шаг 3. Внутри RandomBoardFillStrategy(...) для каждой клетки игрового поля вычисляется её значение
+// Шаг 4. В соответствии с сгенерированными значениями, строится и возвращается новая структура Board с обновленным значениями для клеток игрового поля
